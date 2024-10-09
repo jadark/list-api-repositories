41af1d734d53394e385b99cf667b107796a6b427
Javier Castro <javiercastro.ik@gmail.com>
2024-10-09 10:35:27 -0500

    fix inde.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
-
-app.listen(3000, () => console.log('Server ready on port 3000.'));
-
-module.exports = app;
